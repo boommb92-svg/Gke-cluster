67838a2eb6592c599f0ea31237061790abad3583
boommb92-svg <boommb92@gmail.com>
2025-11-17 17:13:46 +0530

    Update variable.tf

--- main/variable.tf
+++ main/variable.tf
@@ -1,43 +1,52 @@
 variable "project" {
-  type = string
+  description = "GCP project ID"
+  type        = string
 }
 
 variable "region" {
-  type    = string
-  default = "us-central1"
+  description = "GCP region (for provider)"
+  type        = string
+  default     = "us-central1"
 }
 
 variable "location" {
-  type    = string
-  default = "us-central1"
+  description = "GKE location: region (e.g. us-central1) or zone (e.g. us-central1-a)"
+  type        = string
+  default     = "us-central1"
 }
 
 variable "cluster_name" {
-  type    = string
-  default = "tf-gke-cluster"
+  description = "GKE cluster name"
+  type        = string
+  default     = "tf-gke-cluster"
 }
 
 variable "node_count" {
-  type    = number
-  default = 2
+  description = "Initial nodes in the node pool"
+  type        = number
+  default     = 2         # reduced default
 }
 
 variable "node_machine_type" {
-  type    = string
-  default = "e2-medium"
+  description = "GCE machine type for nodes"
+  type        = string
+  default     = "e2-medium"
 }
 
 variable "node_disk_size_gb" {
-  type    = number
-  default = 100
+  description = "Node disk size in GB (pd-standard)"
+  type        = number
+  default     = 50        # reduced default
 }
 
 variable "node_min_count" {
-  type    = number
-  default = 1
+  description = "Autoscaler minimum"
+  type        = number
+  default     = 1
 }
 
 variable "node_max_count" {
-  type    = number
-  default = 5
+  description = "Autoscaler maximum"
+  type        = number
+  default     = 5
 }
