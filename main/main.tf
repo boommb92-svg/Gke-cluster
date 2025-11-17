67f5b3abd54d275fbad8afb1bec24579d6b36d5c
boommb92-svg <boommb92@gmail.com>
2025-11-18 02:01:56 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -37,7 +37,6 @@ resource "google_container_cluster" "primary" {
   project  = var.project
 
   deletion_protection = false
-
   remove_default_node_pool = true
   initial_node_count       = 1
 
@@ -59,6 +58,7 @@ resource "google_container_cluster" "primary" {
   ]
 }
 
+
 # ------------------------------
 #   SEPARATE NODE POOL
 # ------------------------------
@@ -71,10 +71,10 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = var.node_count
 
   node_config {
-    machine_type  = var.node_machine_type
-    disk_size_gb  = var.node_disk_size_gb
-    disk_type     = "pd-standard"
-    
+    machine_type       = var.node_machine_type
+    disk_size_gb       = var.node_disk_size_gb
+    disk_type          = "pd-standard"
+    enable_external_ips = false   # <- THIS IS THE KEY FIX
   }
 
   management {
