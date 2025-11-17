8f7eab2d894f8602cd474928cd6c6df59fc37bea
boommb92-svg <boommb92@gmail.com>
2025-11-17 16:44:21 +0530

    Update outputs.tf

--- main/outputs.tf
+++ main/outputs.tf
@@ -1,1 +1,12 @@
+output "cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+output "get_credentials" {
+  value = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region=${var.location} --project=${var.project}"
+}
 
